8173303400cd7c2e45d8ff5e57b47fdbec7c06f5
SF1960 <76819375+SF1960@users.noreply.github.com>
2022-07-09 07:51:05 +0100

    Update InfraRedCode.py

--- InfraRedCode.py
+++ InfraRedCode.py
@@ -1,1 +1,14 @@
 ## Infrared code basics
+
+import time
+import os
+
+DELAY_TIME = 0.5
+
+time.sleep(DELAY_TIME);
+
+
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO. This is probably because you need superuser. Try running again with 'sudo'.")
